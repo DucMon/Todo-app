aa25bf35f9b521a6cef033318a60bf22b81c8759
Nguyễn Khánh Đức <khanhduc1998123@gmail.com>
2018-11-13 15:57:33 +0700

    [Update] Feature Schedule+Goal

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // to-do list
-var ul = document.getElementById("todo-list");
+var ultodo = document.getElementById("todo-list");
+var ulSchedule = document.getElementById("schedule-list");
+var ulGoal = document.getElementById("goal-list");
 var dataTodoList = [];
+var dataGoalList = [];
+var dataScheduleList = [];
 
-function createNew() {
-    var input = document.getElementById("userInput");
+function createNewToDo() {
+    var input = document.getElementById("userInputToDo");
     if (input.value != "") {
         dataTodoList.push({
             name: input.value,
@@ -11,15 +15,73 @@ function createNew() {
         });
     }
     input.value = "";
-    renderContent(dataTodoList);
+    renderContentToDo(dataTodoList);
 }
 
-function renderContent(data) {
+function createNewGoal() {
+    var input = document.getElementById("userInputGoal");
+    if (input.value != "") {
+        dataGoalList.push({
+            name: input.value,
+            isCompleted: false
+        });
+    }
+    input.value = "";
+    renderContentGoal(dataGoalList);
+}
+function createNewSchedule() {
+    var input = document.getElementById("userInputSchedule");
+    var Hour = document.getElementById("Hour").value;
+    var Minute = document.getElementById("Minute").value;
+    var Meridiem = document.getElementById("Meridiem").value;
+    var time = Hour +":"+ Minute +" "+ Meridiem;
+    if (input.value != "") {
+        dataScheduleList.push({
+            name: input.value,
+            time: time
+        });
+    }
+    input.value = "";
+    renderContentSchedule(dataScheduleList);
+}
+function renderContentSchedule(data) {
     var content = data.map(function(item, index) {
         var isCompleted = "";
-        var icon = "\u00D7";
+        var icon = "<i class='fa fa-close' aria-hidden='true'></i>";
         if (item.isCompleted) {
-            icon = "\u2713";
+            icon = "<i class='fa fa-check' aria-hidden='true'></i>";
+            isCompleted = "isCompleted";
+        }
+
+        return "<li><b>" + item.time + "</b> " + item.name +
+            "<span id ='close" + index + "' class = 'close' onclick = 'deleteSchedule(event," + index + ")' >" + icon + "</span>" + "</li>";
+    });
+    console.log(content);
+    ulSchedule.innerHTML = content.join("");
+}
+
+function renderContentGoal(data) {
+    var content = data.map(function(item, index) {
+        var isCompleted = "";
+        var icon = "<i class='fa fa-close' aria-hidden='true'></i>";
+        if (item.isCompleted) {
+            icon = "<i class='fa fa-check' aria-hidden='true'></i>";
+            isCompleted = "isCompleted";
+        }
+
+        return "<li class='" + isCompleted + "' onclick = 'finishGoal(" + index + ")'>" + item.name +
+            "<span id ='close" + index + "' class = 'close' onclick = 'deleteItemGoal(event," + index + ")' >" + icon + "</span>" + "</li>";
+    });
+    console.log(content);
+    ulGoal.innerHTML = content.join("");
+}
+
+function renderContentToDo(data) {
+    var content = data.map(function(item, index) {
+        var isCompleted = "";
+        var icon = "<i class='fa fa-close' aria-hidden='true'></i>";
+        if (item.isCompleted) {
+            icon = "<i class='fa fa-check' aria-hidden='true'></i>";
             isCompleted = "isCompleted";
         }
 
@@ -27,20 +89,34 @@ function renderContent(data) {
             "<span id ='close" + index + "' class = 'close' onclick = 'deleteItem(event," + index + ")' >" + icon + "</span>" + "</li>";
     });
     console.log(content);
-    ul.innerHTML = content.join("");
+    ultodo.innerHTML = content.join("");
 }
-
+// finishitem
 function finishWork(index) {
     var index = parseInt(index);
     dataTodoList[index].isCompleted = true;
-    renderContent(dataTodoList);
+    renderContentToDo(dataTodoList);
 }
-renderContent(dataTodoList);
-
+function finishGoal(index) {
+    var index = parseInt(index);
+    dataGoalList[index].isCompleted = true;
+    renderContentGoal(dataGoalList);
+}
+// deleteItem
 function deleteItem(e, index) {
     e.stopPropagation();
     dataTodoList.splice(index, 1);
-    renderContent(dataTodoList);
+    renderContentToDo(dataTodoList);
+}
+function deleteSchedule(e, index) {
+    e.stopPropagation();
+    dataScheduleList.splice(index, 1);
+    renderContentSchedule(dataScheduleList);
+}
+function deleteItemGoal(e, index) {
+    e.stopPropagation();
+    dataGoalList.splice(index, 1);
+    renderContentGoal(dataGoalList);
 }
 // clock
 var monthNames = ["January", "February", "March", "April", "May", "June",
